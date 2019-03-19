edb528f81bbd44ae5fb786e9e305ce23320c44df
Hunter Kaleb Perry <skittles2519@hotmail.com>
2019-03-18 21:36:18 -0600

    fixes for website 1

--- lesson-10/scripts/weatherapi.js
+++ lesson-10/scripts/weatherapi.js
@@ -1,8 +1,7 @@
 
-var requestURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fc0587bc76f9c5a27585b00097149b4f&units=imperial ';
+var requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fc0587bc76f9c5a27585b00097149b4f&units=imperial ';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL, true);
-    request.responseType = 'json';
 request.send();
 request.onload = function() {
    
@@ -11,7 +10,7 @@ request.onload = function() {
 
     function populateHeader(jsonObj) {
         
-    var main = jsonObj['main']
+    var main = jsonObj['main'];
     document.getElementById("main").innerHTML = main;
     document.getElementById("temp_max").innerHTML = main.temp_max;
     document.getElementById("humidity").innerHTML = main.humidity;
