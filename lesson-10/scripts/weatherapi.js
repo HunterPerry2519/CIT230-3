7e162f90f5f20d883768d091f7023612433449eb
Hunter Kaleb Perry <skittles2519@hotmail.com>
2019-03-19 00:05:04 -0600

    more website changes 2

--- lesson-10/scripts/weatherapi.js
+++ lesson-10/scripts/weatherapi.js
@@ -1,21 +1,14 @@
 
-var requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fc0587bc76f9c5a27585b00097149b4f&units=imperial ';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL, true);
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=fc0587bc76f9c5a27585b00097149b4f&units=imperial' , true);
 request.send();
 request.onload = function() {
    
-    var Main = request.response;
-  populateHeader(Main);
-
-    function populateHeader(jsonObj) {
-        
-    var main = jsonObj['main'];
-    document.getElementById("main").innerHTML = main;
-    document.getElementById("temp_max").innerHTML = main.temp_max;
-    document.getElementById("humidity").innerHTML = main.humidity;
-    document.getElementById("wind").innerHTML = main.wind;
+    var Main = JSON.parse(request.responseText);     
+    document.getElementById('main').innerHTML = Main.main;
+    document.getElementById('temp_max').innerHTML = Main.main.temp_max;
+    document.getElementById('humidity').innerHTML = Main.main.humidity;
+    document.getElementById('wind').innerHTML = Main.main.wind;
     }
 
 
-}
